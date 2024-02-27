a479e09c7c8f7c1db29661caf0b0b9716e5be3a0
milfer0516 <milfer0516@gmail.com>
2024-02-27 18:42:47 -0500

    Agrego carpeta en la ruta de images

--- src/config/CardsVehiculos.jsx
+++ src/config/CardsVehiculos.jsx
@@ -1,31 +1,35 @@
-
+import imageCamioneta from '../images/Images-Vehiculos/image-camoineta2.jpg'
+import imageCamionetaGlory from '../images/Images-Vehiculos/image-camoineta-glory.jpg'
+import imageVanDe8 from '../images/Images-Vehiculos/imagen-van-8.png'
+import imageVanDe15 from '../images/Images-Vehiculos/image-van14.jpg'
+import imageVanDe19 from '../images/Images-Vehiculos/image-van19.jpg'
 
 const CardsVehiculos = [
    //image-camoineta-glory.jpg
 
     {
         name: "Camioneta para 4 pasajeros",
-        image: "../images/image-camoineta2.jpg",
+        image: {imageCamioneta},
         desciption: 'Camioneta de 4 pasajeros comoda en muy buen estado'
     },
     {
         name: "Camioneta para 6 pasajeros",
-        image: "../images/image-camoineta-glory.jpg",
+        image: {imageCamionetaGlory},
         desciption: 'Camioneta de 4 pasajeros comoda en muy buen estado'
     },
     {
         name: "Van de 8 pasajeros",
-        image: "../images/imagen-van-8.png",
+        image: {imageVanDe8},
         desciption: 'Camineta de 4 pasajeros comoda en muy buen estado'
     },
     {
         name: "Van de 15 pasajeros",
-        image: "../images/image-van14.jpg",
+        image: {imageVanDe15},
         desciption: 'Camineta de 4 pasajeros comoda en muy buen estado'
     },
     {
         name: "Van de 19 pasajeros",
-        image: "../images/image-van19.jpg",
+        image: {imageVanDe19},
         desciption: 'Camineta de 4 pasajeros comoda en muy buen estado'
     },
     
